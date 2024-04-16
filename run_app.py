ed3480017cd0a037506f58ed652dee39b77f8757
Benjamin Post <post_benjamin@yahoo.com>
2024-04-16 16:12:12 -0400

    testing time algorithm

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@
 
 import pickle
 from dataclasses import dataclass
-from typing import Optional
+from time import time
 
 
 @dataclass
@@ -11,7 +11,8 @@ class SaveData:
 
     round_min: int
     goal_time: float
-    time_records: Optional[dict] = None
+    tracking_active: bool
+    time_records: dict
 
 
 def load_data() -> SaveData:
@@ -20,11 +21,15 @@ def load_data() -> SaveData:
         with open("savestate", "rb") as f:
             temp_data = pickle.load(f)
             if not isinstance(temp_data, SaveData):
-                temp_data = SaveData(round_min=15, goal_time=8)
+                temp_data = SaveData(
+                    round_min=15, goal_time=8, tracking_active=False, time_records={}
+                )
                 save_data(temp_data)
 
     except FileNotFoundError:
-        temp_data = SaveData(round_min=15, goal_time=8)
+        temp_data = SaveData(
+            round_min=15, goal_time=8, tracking_active=False, time_records={}
+        )
         save_data(temp_data)
     return temp_data
 
@@ -35,10 +40,34 @@ def save_data(current_data: SaveData) -> None:
         pickle.dump(current_data, f)
 
 
+def add_timestamp(task_name: str, current_data: SaveData) -> None:
+    """Adds a value to the time_record dictionary for the current time and given task name and saves updated user data"""
+    current_data.time_records[time()] = task_name
+    save_data(current_data)
+
+
 def main():
     """main function"""
     current_data = load_data()
-    print(current_data)
+    add_timestamp(input("type something: "), current_data)
+    add_timestamp(input("type something else: "), current_data)
+
+    print(
+        int(
+            (
+                max(current_data.time_records.keys())
+                - min(current_data.time_records.keys())
+            )
+            // 60
+        )
+    )
+    time_list = list(current_data.time_records.keys())
+    print(
+        [
+            [current_data.time_records[x], y - x]
+            for x, y in zip(time_list, time_list[1:])
+        ]
+    )
 
 
 if __name__ == "__main__":
